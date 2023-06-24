213025299dd98e78e9e21bce88fcd51f2f51c099
Joshua <joshuadrc@gmail.com>
2023-06-24 11:41:21 -0500

    Update lucky vec to use Vec4

--- dtypes.py
+++ dtypes.py
@@ -4,11 +4,12 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class Vec3:
+class Vec4:
     x: float
     y: float
     z: float
+    w: float
 
 
-def get_lucky_vec() -> Vec3:
-    return Vec3(x=1.0, y=2.0, z=3.0)
+def get_lucky_vec() -> Vec4:
+    return Vec4(x=1.0, y=2.0, z=3.0, w=4.0)
--- ops.py
+++ ops.py
@@ -1,14 +1,14 @@
 """Library operations."""
 
 from .auth import _check_token
-from .dtypes import Vec3
+from .dtypes import Vec4
 
 
-def add_vec(token: str, v1: Vec3, v2: Vec3) -> Vec3:
+def add_vec(token: str, v1: Vec4, v2: Vec4) -> Vec4:
     _check_token(token)
-    return Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z)
+    return Vec4(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w)
 
 
-def mag_vec(token: str, v: Vec3) -> float:
+def mag_vec(token: str, v: Vec4) -> float:
     _check_token(token)
-    return (v.x * v.x + v.y * v.y + v.z * v.z) ** 0.5
+    return (v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w) ** 0.5
